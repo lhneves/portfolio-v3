4a8336a0cd7d3cd349077eeb96cb6798fcbcb43a
Luiz Doretto <luiz.n.doretto@gmail.com>
2024-05-03 22:45:54 -0300

    feat: create about section and add it to index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from '@/components/Sections/Hero';
+import { About } from '@/components/Sections/About';
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
       <Hero />
+      <About />
     </main>
   );
 }
